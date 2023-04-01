e3f3a4a5482add23edacf1def4e753ada3383115
ald4-rgb <dataem2026a@gmail.com>
2023-04-01 08:58:39 -0600

    Validar Strings

--- Strings/ValidarStrings.java
+++ Strings/ValidarStrings.java
@@ -32,7 +32,7 @@ public class ValidarStrings {
             //vamos a hacer el ejemplo dentro de un if
             // si es distinto es null
             if (esNulo == false ) {
-                //si es falso convertimo el valor convertido a mayusculas
+                //si es falso convertimos el valor convertido a mayusculas
                 System.out.println("curso = " + curso.toUpperCase());
                 System.out.println(curso.concat(" desde cero!"));
             }
@@ -55,36 +55,85 @@ public class ValidarStrings {
             // lo dejaremos ejecutar todo como variables ya que es lo mismo
             if(esNulo){
                 //si se cumple la condición le damos un valor por defecto
-               // curso = "Apigee con el profesor Chubaldo";
+                // podemmos comentar y descometnar  curso
+                curso = "Apigee con el profesor Chubaldo";
 
-               // ¿Qque pasa si curso es un String vacio
-                curso = ""; // existe el bojeto tieene referencia pero no tiene contenido pero no da error
+               // ¿Que pasa si curso es un String vacio
+                // ¿Que pasa si curso le damos un spacio en blanco?
+                // deja de levantar
+                // vacio sin espacio
+                //curso =""; //pueedes descomentar
+                //descometna si quieres ver como funciona el metodo isBlank() con un espacio
+               //  curso = " "; // existe el objeto tieene referencia pero no tiene contenido pero no da error. Puedes descomentarlo
             }
 
-            // ¿como podriamso validar que un String tenga un tamaño?
-            //  entonces esvacio es igual a curso.length() el metodo length() retorna un Integet
-            // entonces con eso podemos saber si la cantidad de caracteres es igual a cero  es por que es vacio
-             boolean esvacio = curso.length() ==  0 ;
-            //esto lo podemos imprimir  siempre y cuando no sea vacio sea dsitinto  de vacio
-            // este if(esvacio == false){...} tambien se puede escribir de esta forma if(!esvacio){...}
-            // si es distinto a vacio podemso imprimir
-            // tambien podemos hacerlo de este moodo if(curso.length() ==  0){} o if(curso.length() >  0){}
 
-            if(!esvacio){
-                System.out.println(curso.toUpperCase());
-                System.out.println("Bienvenido al curso\s".concat(curso));
-                // es vacio como vemos este bloque no se ejecuta nunca de va a ejecutar
-            }
 
-        System.out.println("***************************Descomentar**************************************");
-            //entonce que pasa si ponemos el metodo toUpperCase() lo convierte a mayusculas
-            System.out.println(curso.toUpperCase());
-            // a qui lo imprimimos sin que sea mayusculas podemos concatenar incluso con el concat()
+ /////////////////////////////////////////Validaccion su es vasio un String//////////////////////////////////////////////
+
+        boolean esVacio = curso.length() ==  0 ;
+        //esto lo podemos imprimir  siempre y cuando no sea vacio sea dsitinto  de vacio
+        // este if(esvacio == false){...} tambien se puede escribir de esta forma if(!esvacio){...}
+        // si es distinto a vacio podemso imprimir
+        // tambien podemos hacerlo de este moodo if(curso.length() ==  0){} o if(curso.length() >  0){}
+        System.out.println("esvacio = " + esVacio);
+
+
+        /////////////////////////////////////otra forma con el metodo isEmpity()/////////////////////////////////
+
+        //String tambien tiene un  otro atajo que es  isEmpity() qye al final hace exactamente lo mismo
+
+        //creamos el boolean
+        boolean  esVacio2= curso.isEmpty();
+
+        System.out.println("esVacio2 ="+esVacio2);
+        //entonces negamo si es  esVacio2 == false
+
+
+        if(!esVacio){
+           // System.out.println(curso.toUpperCase());
+           //System.out.println("Bienvenido al curso\s".concat(curso));
+            // es vacio como vemos este bloque no se ejecuta nunca de va a ejecutar
+        }
+        if(esVacio2 == false){
+
+            //mostramos el vacio 2 si nos fiajos el resutlado es igual al anteroir
+        //    System.out.println(curso.toUpperCase());
             System.out.println("Bienvenido al curso\s".concat(curso));
+        }
+        /////////////////uso el metodo isBlank()/////////////////////////////////////////////////////////////
+
+        //isBlank() --> valida que sea distinto de null  tambien valida que swa distinto de empty ademas valida
+        // que tenga contenido que sea distinto de un espacio en blanco
+
+        boolean esBlanco= curso.isBlank();
+
+        System.out.println("esBlanco = " + esBlanco);
+
+        //etnocnes si esblanco == false if(!esBlanco) {...} recordesmo que no se ejecute lo omite ya que lo esta validando
+        // y  si descomentamos lo imprime deja de ser blanco
+        if(!esBlanco){
+            System.out.println(curso.toUpperCase());
+            System.out.println("Bienvenido al curso\s".concat(curso));
+        }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+        System.out.println("***************************Descomentar curso**************************************");
+        //entonce que pasa si ponemos el metodo toUpperCase() lo convierte a mayusculas
+        System.out.println(curso.toUpperCase());
+        //a qui lo imprimimos sin que sea mayusculas podemos concatenar incluso con el concat()
+        System.out.println("Bienvenido al curso\s".concat(curso));
+        //¿como podriamso validar que un String tenga un tamaño?
+        //entonces esvacio es igual a curso.length() el metodo length() retorna un Integet
+        //entonces con eso podemos saber si la cantidad de caracteres es igual a cero  es por que es vacio
+
+      /*puedes descomentar y comentar para ver como funcionan estas validaciones
+      *
+      *
+      *  */
 
 
             }
-
 }
