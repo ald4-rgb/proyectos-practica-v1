6b82c10ebfbdcdaf938719ac9014f6f38d734be8
ald4-rgb <dataem2026a@gmail.com>
2023-04-04 08:16:45 -0600

    Se agrega metodo charAt() y se completa con el comparaTo()

--- Strings/MetodosStrings.java
+++ Strings/MetodosStrings.java
@@ -24,8 +24,25 @@ public class MetodosStrings {
         //vamos a probar si lo cambio ya que es distinto Chubaldo con mayusqula que con minuscula
         //recordemos que java es sencible a minusculas y mayusculas
         System.out.println("nombre.equals(\"chubaldo\") = " + nombre.equals("chubaldo"));
-        //¿pero que pasa con? equalsIgnoreCase()
+        //¿pero que pasa con? equalsIgnoreCase() ignora la mayusucla o minuscula
         System.out.println("nombre.equalsIgnoreCase(\"chubaldo\") = " + nombre.equalsIgnoreCase("chubaldo"));
+        //otro metodo parecido al equals para comparar  caracteres comapreTo("...."); el oreden de este metodo es lexico grafico que esta basado en orden
+        // sobre el valor de la tabla unicode en otras palabras realiza comparacion sobre el orden numerico  de cada codigo  que esta en la tabla unicode
+        // va comparando estos caracteres y los va ordenando y a esto se le llama orden lexico grafico por ejemplo chubaldo compareTO("ubaldo") si el valor
+        // da cero son identicos .
+        System.out.println("nombre.compareTo(\"Chubaldo\") = " + nombre.compareTo("Chubaldo"));
+        //vamos a comparar con otro nombre
+        //como vemos asigna un ordenamiento deacurdo a la tabla unicode lo va ordenando segun un entero
+        // recordemos que compareTo("...") va a tener siempre tendra int esta basado en los valores de cada caracter en la tabla unicode
+        System.out.println("nombre.compareTo(\"Ubaldo\") = " + nombre.compareTo("Ubaldo"));
+        //otro metodo el charAt() nos permite convertir un String en caracteres
+        // podemos tener un caracter en particular lo que recibe este emtodo es un valor entero en el metodo  un integer
+        // y retorna un caracter entonces por ejemplo 0 correcponde a la C
+        // y si quiero la h seria el uno
+        // y si quiero el ultimo caracter seria el 7
+        System.out.println("nombre.charAt( ) = " + nombre.charAt(0) +"\nnombre.charAt( ) = " + nombre.charAt(1)+"\nnombre.charAt( ) = " + nombre.charAt(7));
 
+
+        
     }
 }
