b173e30eac7b3c64b0b1abbb1ef4c7e8b2538b94
ald4-rgb <62630597+ald4-rgb@users.noreply.github.com>
2023-05-16 21:49:07 -0600

    Presedencia operadores 2 parte

---
+++ ejericios-practicos-una-sola-calse-v1/EstanqueGasolina.java
@@ -0,0 +1,46 @@
+
+/*
+* Suponiendo un estanque de gasolina (gas) con capacidad 70 litros, se requiere un programa que pida la medida actual en litros y mostrar el resultado de la forma: Insuficiente, Suficiente, Medio...
+
+La medida o capacidad actual del estanque puede ser en tipo double, para una mejor precisión, pero tambien puede ser del tipo int.
+
+Si la capacidad actual es 70 litros: imprimir Estanque lleno
+
+Si está entre 60 y menor a 70: imprimir Estanque casi lleno
+
+Si está entre 40 y menor a 60: imprimir Estanque  3/4
+
+Si está entre 35 y menor a 40: imprimir Medio Estanque
+
+Si está entre 20 y menor a 35: imprimir Suficiente
+
+Si está entre 1 y menor a 20: imprimir Insuficiente
+* */
+
+public class EstanqueGasolina {
+    public static void main(String[] args) {
+        double capacidadMaxima = 50;
+
+        String tanquisito=  capacidadMaxima == 70?"tanque lleno":(capacidadMaxima >=60 &&
+                            capacidadMaxima <70)?"casi lleno":""  ;
+
+        System.out.println(tanquisito);
+
+       /* Una forma con if-else
+
+        if(capacidadMaxima ==70){
+            System.out.println("tanque lleno");
+        }else if(capacidadMaxima >= 60 && capacidadMaxima <70){
+            System.out.println("casi lleno");
+        }else if(capacidadMaxima >= 40 && capacidadMaxima <60){
+            System.out.println("Estanque  3/4");
+        }else if(capacidadMaxima >= 35 && capacidadMaxima <40){
+            System.out.println("Medio Estanque");
+        }else if(capacidadMaxima >= 20 && capacidadMaxima <35){
+            System.out.println("Suficiente");
+        } else if (capacidadMaxima >=1 && capacidadMaxima < 10){
+            System.out.println("insuficiente");
+        }*/
+
+    }
+}
---
+++ ejericios-practicos-una-sola-calse-v1/OrdenDosNumeros.java
@@ -0,0 +1,14 @@
+
+    /*
+    * El desafío es un programa que pida dos números y los muestre ordenados de mayor a menor.
+    * Podría ser utilizando operador ternario.*/
+public class OrdenDosNumeros {
+        public static void main(String[] args) {
+            int num1= 8;
+            int num2= 2;
+
+            String mayorMenor = (num1>num2)?num1 + " , "+num2:num2 +" & " + num1 ;
+            System.out.println("mayorMenor = " + mayorMenor);
+
+        }
+}
