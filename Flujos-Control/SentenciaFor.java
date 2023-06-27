65f84d7447577a69509950252fa93557d63b27d4
ald4-rgb <dataem2026a@gmail.com>
2023-06-27 01:33:49 -0600

    explicacion del for

---
+++ Flujos-Control/SentenciaFor.java
@@ -0,0 +1,20 @@
+/*
+* Nos permite trabajar con bluces o ciclos en java son setnecias de control repetitivas
+* iteraciones que modifican  el flujo secuencial de nuestra app
+* pero ejecutando una o mas veces un bloque de codigo
+* de forma reiterativa en donde se tiene que conocer los limites del cliclo
+* del for el valor inicial de la iteración cunado comienza y cuando termina
+* es decir cuantas veces se va reptir este fragmento de codigo
+* */
+
+public class SentenciaFor {
+    public static void main(String[] args) {
+
+        //la palabra  clave  o reservada  y va con parentesis
+        //pero el for esta separado por 3 partes con ;
+        for(;;){
+            System.out.println("2222");
+        }
+
+    }
+}
