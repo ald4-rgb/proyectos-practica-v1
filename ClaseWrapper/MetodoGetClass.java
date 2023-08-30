c128270572623a07f3456c459194c55c55d3d71b
ald4-rgb <dataem2026a@gmail.com>
2023-08-30 00:27:45 -0600

    ACtualizació de MetodoGetClass itetarion desde donde viene cada clase clases padres

--- ClaseWrapper/MetodoGetClass.java
+++ ClaseWrapper/MetodoGetClass.java
@@ -11,6 +11,8 @@
 *
 * */
 
+import java.lang.reflect.Method;
+
 public class MetodoGetClass {
     public static void main(String[] args) {
 
@@ -31,9 +33,56 @@ public class MetodoGetClass {
         System.out.println("str.getSimpleName()= " + str.getSimpleName());
         //tmabién tenemos getPackage() --> retonar el objeto  package java.
         System.out.println("str.getPackage() = " + str.getPackage());
-        System.out.println("str.getPackage() = " + str.getPackageName());
+        System.out.println("str.getPackageName() = " + str.getPackageName());
 
+        System.out.println("******************************************+Listodo con getMethods()");
+        /*Tambien podemos mostrar cuales son sus cmapos sus métodos sus atributos
+        * simplemente usanndo un forech el tipo de datos que retorna
+        * si quiero recorrer  los metodos por ejemplo si quiero recorrer getMethod
+        * en el foreach siempre tiene que ir el tipo de los elementos del arreglo
+        */
+        for (Method method :str.getMethods() ) {
+            //quiero imprimir el metodo  pero el metodo es un objeto  quiero
+            // imprmir el nombre del método entonces invocamos el metodo getName()
+            //esto va a listar todos los método de la clase String cada uno
+            System.out.println("method = " + method.getName());
 
+            //entonces el método getClass nos retorna infromación sobre el objeto
+            //su estructura interna de la clase
+        }
+
+        System.out.println("***************Ejemplo con Integer**********************************************");
+
+        //por ejemplo Integer num
+        Integer num = 34;
+        //Class Integre class igual a num.getClass()
+        //entonces obtenemos el objeto class del objeto Integer
+        Class intClass = num.getClass();
+        Class objClass =  intClass.getSuperclass().getSuperclass();
+        //imprmimor el objeto
+        System.out.println("intClass = " + intClass);
+        //imprmiendo el nombre completo
+        System.out.println("getName() = " + intClass.getName());
+        //imprmiendo el nombre getPAckageName()
+        System.out.println("intClass.getPackageName() = " + intClass.getPackageName());
+        //por ejemplo Superclass ¿Cual es la super clase? o la clase padre recordemos que todos lo objetos en java heredan de object
+        //pero a su vez Integer Hereda  de Number
+        System.out.println("intClass.getPackageName() = " + intClass.getSuperclass());
+        //ahora a esta clase q  ue es Number quiero ver su clase padre deberia ser object
+        //entonces de forma encadenada  getSuperclass().getSuperclass() al integer retorna Number y Number le otorgamos el método Superclas
+        System.out.println("intClass.getPackageName() = " + objClass);
+
+        System.out.println("******************Iteración por nombre de los metoso");
+
+        //¿Que pasa si quiero mostrar todo los metodos de la clase objClass?
+        for (Method methods: objClass.getMethods() ) {
+            //aqui va imprmir el nombre todos los metodos de la clase objClass
+            System.out.println("methods.getName() = " + methods.getName());
+        }
+
+     /*Entonces de esta forma podemos hacer una relfexión que es como un espejo de la erstructura Interna de los objetos
+     * Un Api muy completa getCLass
+     * */
 
     }
 }
