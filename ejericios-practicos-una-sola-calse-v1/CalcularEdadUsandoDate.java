abeaceecc9181d3d8ceed80213180fc5b428128f
ald4-rgb <dataem2026a@gmail.com>
2024-02-05 04:15:57 -0600

    ejericio resulto con date

--- ejericios-practicos-una-sola-calse-v1/CalcularEdadUsandoDate.java
+++ ejericios-practicos-una-sola-calse-v1/CalcularEdadUsandoDate.java
@@ -6,34 +6,34 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 public class CalcularEdadUsandoDate {
     public static void main(String[] args) throws ParseException {
+    
+        String IngresoFecha ;
 
-        //////////////ejemplito instructor
+        System.out.println(" Ingrese su fecha de nacimiento ");
+        Scanner sc = new Scanner(System.in);
+        IngresoFecha = sc.next();
+       //uso de la clase Date para
+        SimpleDateFormat personalizar = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date fechaNac = personalizar.parse(IngresoFecha);
+
+        System.out.println("Mi fecha de nacimiento= " + fechaNac);
+
+        Date fechaActual = new Date();
+
+        personalizar = new SimpleDateFormat("yyyyMMdd");
+        Integer nacio = Integer.parseInt(personalizar.format(fechaNac));
+        Integer actualemnte = Integer.parseInt(personalizar.format(fechaActual));
+
+        int resultado =  (actualemnte - nacio) /10000;
+
+        System.out.println("resultado = " + resultado);
         
-        // String ingresarFecha = "1995-10-11";
-
-        // SimpleDateFormat dateCustom = new SimpleDateFormat("yyyy-MM-dd");
-
-        // Date custom = dateCustom.parse(ingresarFecha);
-
-        // Date actula = new Date();
-
-        // dateCustom = new SimpleDateFormat("yyyyMMdd");
-
-        // Integer desde = Integer.parseInt(dateCustom.format(custom));
-        // Integer hoy = Integer.parseInt(dateCustom.format(actula));
-
-        // int calculo = (hoy - desde) / 10000;
-
-        // System.out.println(calculo);
-
-
-
-
-
-
+        
     }
 
 }
