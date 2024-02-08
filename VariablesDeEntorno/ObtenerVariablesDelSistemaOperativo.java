a556bea9d39ece6aed83a88405ec02f1c556c3c8
ald4-rgb <dataem2026a@gmail.com>
2024-02-08 07:54:55 -0600

    actualizacion sacar varaibles de entorno del sistema

---
+++ VariablesDeEntorno/ObtenerVariablesDelSistemaOperativo.java
@@ -0,0 +1,60 @@
+
+/*
+*
+* Todos los sistemas operativos manejan variables de entorno para configuraciones
+* por ejemplo para registrar rutas de nuestras apps,directorios parametros de cualquier
+* tipo por ejemplo cuando instalamos java instalamos JAVA_HOME para configurar la ruta
+* base o raiz del jdk y tambien tenemos la variable de entorno path donde guardamos y regsitramos
+* rutas de nuestras apilaciones para poder ejecutar comandos en la terminal
+*
+* ¿Como acceder a estas variables de ambinte?
+*
+*   Accederemos a estas variables de ambiente desde java utilizando la clase System con el metodo getenv()
+* */
+
+import java.util.Map;
+
+public class ObtenerVariablesDelSistemaOperativo {
+    public static void main(String[] args) {
+        //este método getEnv() de la  clase System va a retornar un objeto del tipo Map<K,V> de java
+        //con todas las variables de entorno
+        //Map<k,j> : un map en java es como un arreglo asociativo que asocia valores que están guardados con un nombre
+        //entonces guardamos elementos objetos de cualquier tipo y lo podemos asignar un nombre entonces se le
+        //conoce ccomo llave y valor.
+        // tenemos que indicar con los paretnevis triangulares los tipos de datos donde el nombre es de tipo String
+        // y el valor tambien
+        // Map<String,String> valEnv = System.getenv();
+        Map<String,String> valEnv = System.getenv();
+
+        //pintamos varaibles de ambeinte o entronos del sistema
+        System.out.println("Varaibles de ambeinte del sistema = " + valEnv);
+
+        //como puedo obtener una en particular tambien con el mismo metodo System.getenv();
+        //pero pasamos el nombre de la varaibel de entorno por argumento
+        String username = System.getenv("USERNAME");
+
+        System.out.println("username = " + username);
+        //ahora veamos JAVA_HOME tiene que ser el mismo nombre de la varaibel de ambiente
+        String javaHome = System.getenv("JAVA_HOME");
+        System.out.println("JAVA_HOME = " + javaHome);
+        //otra más por ejemplo tempDir
+        String temDir = System.getenv("TEMP");
+        System.out.println("temDir = " + temDir);
+
+        //varaible path
+        String path = System.getenv("PATH");
+        System.out.println("PATH = " + path);
+
+        //tambien con el objeto valEnv de la varaible de ambiente de este mapa que obtenemos
+        //a traves de System.getenv(); tambien podemos obtenerlo a travez de ese objeto que creamos
+        // seria valEnv.get("PATH");
+        //entonces valEnv --> el objeto del Map<k,j>
+        //valEnv.get("...") --> el método get del mapa pasamos el nombre o la llave con la cual guardamos o se
+        //guarda esta varaible de entonrnno en el diccionario
+        String path2 = valEnv.get("PATH");
+        System.out.println("PATH2 = " + path2);
+
+
+
+    }
+}
