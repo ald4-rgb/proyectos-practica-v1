753dee1fd48ed0c758de2c48a77a4f87b740ff8e
ald4-rgb <dataem2026a@gmail.com>
2024-02-18 23:55:13 -0600

    pequeño ejemplo de clase inmutable

---
+++ EjemploClaseInmutable/Conocerce.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author shelltrain
+ */
+public class Conocerce {
+    
+    public static void main(String[] args) {
+        
+        Hola hola = Hola.invitados("hola","adios" );
+        
+        
+        System.out.println(hola.getSaludar());
+        System.out.println(hola.getDespedida());
+        
+        
+        
+        
+        
+        
+    }
+}
